68ba5a2e1464289a6e2f41594a62076c1c14e53f
Mark <markh4b@hotmail.com>
2015-09-27 22:25:44 +0200

    Added Tuti

---
+++ Script.c
@@ -0,0 +1,21 @@
+
+static npc_tuti;
+
+func Initialize()
+{
+	CreateTuti();
+}
+
+func InitializePlayer(int player)
+{
+	var crew = GetHiRank(player);
+	crew->SetPosition(80, 1048);
+}
+
+// ----- *** NPC Creation *** -----------------------------------------
+
+func CreateTuti()
+{
+	npc_tuti = CreateObject(Clonk, 695, 1020, NO_OWNER);
+	npc_tuti->SetName("$NPC_Tuti$");
+}
