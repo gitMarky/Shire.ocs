1dbed55d95eaa892794af5d30be43b44595fb6f8
Mark <markh4b@hotmail.com>
2015-11-24 19:33:34 +0100

    Tarydium object

---
+++ Objects.ocd/Items.ocd/Tarydium.ocd/Script.c
@@ -0,0 +1,12 @@
+/*--- Nugget ---*/
+
+protected func Hit(x, y)
+{
+	StonyObjectHit(x,y);
+	return true;
+}
+
+local Collectible = 1;
+local Name = "$Name$";
+local Description = "$Description$";
+local Plane = 470;
