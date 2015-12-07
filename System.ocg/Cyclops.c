e445f134e9801d4ef172246575b0287c219b4375
Mark <markh4b@hotmail.com>
2015-12-07 17:23:32 +0100

    Cleaner code and fire breath for cyclops

--- System.ocg/Cyclops.c
+++ System.ocg/Cyclops.c
@@ -3,44 +3,29 @@ global func AddCyclopsAI(object clonk) // somewhat hacky, but it works
 {
 	var effect_name = "IntCyclopsAI";
 	var fx = GetEffect(effect_name, clonk);
-	if (!fx) fx = AddEffect(effect_name, clonk, 1, 10);
+	if (!fx) fx = AddEffect(effect_name, clonk, 1, 2);
 	if (!fx || !clonk) return nil;
 	
-//	fx.ai = AI;
-//	clonk.ExecuteAI = AI.Execute;
 	clonk.ai = fx;
-//	if (clonk->GetProcedure() == "PUSH") fx.vehicle = clonk->GetActionTarget();
-//	AI->BindInventory(clonk); // this works
+
+	// bin inventory
 	var cnt = clonk->ContentsCount();
 	fx.bound_weapons = CreateArray(cnt);
 	for (var i=0; i<cnt; ++i) fx.bound_weapons[i] = clonk->Contents(i);
 
-//	AI->SetHome(clonk); // this did not
+	// set home
 	fx.home_x = clonk->GetX();
 	fx.home_y = clonk->GetY();
 	fx.home_dir = DIR_Left;
 
-//	AI->SetGuardRange(clonk, fx.home_x-AI_DefGuardRangeX, fx.home_y-AI_DefGuardRangeY, AI_DefGuardRangeX*2, AI_DefGuardRangeY*2);
-	fx.guard_range = {//x=x, y=y, wdt=wdt, hgt=hgt};
+	// set guard range
+	fx.guard_range = {
                       x = fx.home_x-AI_DefGuardRangeX,
                       y = fx.home_y-AI_DefGuardRangeY,
                       wdt = AI_DefGuardRangeX*2,
                       hgt =  AI_DefGuardRangeY*2};
 
 	AI->SetMaxAggroDistance(clonk, AI_DefMaxAggroDistance);
-	// AI editor commands
-	//if (!clonk.EditCursorCommands)
-	//	clonk.EditCursorCommands = [];
-	//else if (clonk.EditCursorCommands == clonk.Prototype.EditCursorCommands)
-	//	clonk.EditCursorCommands = clonk.EditCursorCommands[:];
-	//var idx;
-	//if ((idx=GetIndexOf(clonk.EditCursorCommands, "AI_Add()"))>=0) clonk.EditCursorCommands[idx] = nil;
-	//if (GetIndexOf(clonk.EditCursorCommands, AI.AI_SetHome)<0)
-	//{
-	//	var l = GetLength(clonk.EditCursorCommands);
-	//	clonk.EditCursorCommands[l++] = clonk.AI_SetHome = AI.AI_SetHome;
-	//	clonk.EditCursorCommands[l++] = clonk.AI_BindInventory = AI.AI_BindInventory;
-	//}
 	return fx;
 }
 
@@ -88,10 +73,11 @@ global func CyclopsFindTarget(fx)
 	var hostile_criteria = Find_Hostile(fx.cyclops->GetOwner());
 	if (fx.cyclops->GetOwner() == NO_OWNER)
 		hostile_criteria = Find_Not(Find_Owner(fx.cyclops->GetOwner()));
-//	for (var target in FindObjects(Find_InRect(fx.guard_range.x-fx.cyclops->GetX(),fx.guard_range.y-fx.cyclops->GetY(),fx.guard_range.wdt,fx.guard_range.hgt), Find_OCF(OCF_CrewMember), hostile_criteria, Find_NoContainer(), Sort_Random()))
+
 	for (var target in FindObjects(Find_InRect(fx.guard_range.x,fx.guard_range.y,fx.guard_range.wdt,fx.guard_range.hgt), Find_OCF(OCF_CrewMember), hostile_criteria, Find_NoContainer(), Sort_Random()))
 		if (PathFree(fx.cyclops->GetX(),fx.cyclops->GetY(),target->GetX(),target->GetY()))
 			return target;
+
 	// Nothing found.
 	return;
 }
@@ -113,6 +99,7 @@ global func CyclopsExecuteMelee(fx)
 		fx.weapon=nil;
 		return false;
 	}
+
 	// Are we in range?
 	var x=fx.cyclops->GetX();
 	var y=fx.cyclops->GetY();
@@ -123,21 +110,14 @@ global func CyclopsExecuteMelee(fx)
 	// the cyclops stands in its place and hits objects or breathes fire
 	if (PathFree(x,y,tx,ty))
 	{
+		// hit objects
 		if (Abs(dx) <= 35 && dy >= -15)
 		{
 			// OK, slash!
 			if (fx.cyclops->IsAiming())
 			{
-//				if (!fx.did_aim)
-//				{
-					fx.weapon->ControlUseHolding(fx.cyclops, dx, dy);
-//					fx.did_aim = true;
-//				}
-//				else
-//				{
-					fx.weapon->ControlUseStop(fx.cyclops, dx, dy);
-//					fx.did_aim = false;
-//				}
+				fx.weapon->ControlUseHolding(fx.cyclops, dx, dy);
+				fx.weapon->ControlUseStop(fx.cyclops, dx, dy);
 				return;
 			}
 
@@ -158,6 +138,11 @@ global func CyclopsExecuteMelee(fx)
 			
 			return;
 		}
+		
+		if (Inside(ObjectDistance(fx.cyclops, fx.target), 40, 120))
+		{
+			DoFireBreath(fx, x ,y , tx, ty);
+		}
 	}
 	
 	// Not in range. Walk there.
@@ -177,4 +162,48 @@ global func CyclopsCheckHandsAction(fx)
 	if (!fx.cyclops->HasActionProcedure()) { CyclopsExecuteStand(fx); return false; }
 	// Probably hands busy. Just wait.
 	return false;
+}
+
+global func DoFireBreath(proplist fx, int x, int y, int tx, int ty)
+{
+	var reach = 110;
+	var spray_max = 20;
+	
+	var sx = x - 6;
+	var sy = y - 15;
+
+	// effects
+	if (ObjectDistance(fx.cyclops, fx.target) < reach)
+	{
+		fx.spraying = Min(spray_max, fx.spraying + 1);
+		var angle = Angle(sx, sy, tx, ty);
+//		var sx = +Sin(angle, 15);
+//		var sy = -Cos(angle, 15);
+//		sy -= 3;
+
+		var fuzzy = 10;
+		var velocity = reach; // should take 20 frames to reach the end
+		var vx = +Sin(angle, velocity);
+		var vy = -Cos(angle, velocity);
+
+		var smoke = Particles_Smoke();
+		
+		smoke.R = smoke.G = smoke.B = PV_Linear(255, 100);
+		smoke.Size = PV_Linear(1, PV_Random(20, 30));
+		
+
+		CreateParticle("Fire", sx, sy, PV_Random(vx - fuzzy, vx + fuzzy), PV_Random(vy - fuzzy, vy + fuzzy), PV_Random(15, 20), smoke, 3);
+		CreateParticle("Fire", sx, sy, PV_Random(vx/2 - fuzzy, vx/2 + fuzzy), PV_Random(vy/2 - fuzzy, vy/2 + fuzzy), PV_Random(15, 20), smoke, 3);
+	}
+	else
+	{
+		fx.spraying = 0;
+	}
+	
+	// damage the clonk
+	if (ObjectDistance(fx.cyclops, fx.target) < (spray_max + fx.spraying)*(reach-fuzzy)/(2*spray_max) && this.hero->GetAction() != "Tumble")
+	{
+		var damage=1000;
+		fx.target->DoEnergy(-damage, true, FX_Call_EngGetPunched, NO_OWNER);
+	}
 }
